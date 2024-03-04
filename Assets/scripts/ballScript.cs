e1b40cc69a9a76a6847ad875e0d285facc9ab742
andre13l <up201806860@fe.up.pt>
2024-03-04 00:19:09 +0000

    goal

--- Assets/scripts/ballScript.cs
+++ Assets/scripts/ballScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ballScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidBody;
+    public Rigidbody2D car1;
+    public Rigidbody2D car2;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class ballScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(myRigidBody.position.x < -17.2 || myRigidBody.position.x > 17.2)
+        {
+            myRigidBody.position = new Vector2(0, 0);
+            myRigidBody.velocity = new Vector2(0, 0);
+
+            car1.position = new Vector2(-10, 0);
+            car1.velocity = new Vector2(0, 0);
+
+            car2.position = new Vector2(10, 0);
+            car2.velocity = new Vector2(0, 0);
+        }
+
     }
 }
